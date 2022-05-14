b831c5f527bd0593af142e2c708e84f49f5efad5
PetterBT <petter.bergstrom@iths.se>
2022-05-14 11:12:57 +0200

    Add javadoc

--- src/main/java/TextEditorApp.java
+++ src/main/java/TextEditorApp.java
@@ -1,5 +1,10 @@
 import texteditor.settings.TextEditorBuilder;
 
+
+/**
+ * A simple text-editor built with Java Swing.
+ * @author Petter Bergström
+ */
 public class TextEditorApp {
     public static void main(String[] args) {
         TextEditorBuilder textEditor = new TextEditorBuilder();
