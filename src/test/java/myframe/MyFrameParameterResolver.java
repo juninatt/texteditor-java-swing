8535c8ae04764fa9e4d1177cff3ca740bc6a5786
petterBT <petter.bergstrom@iths.se>
2022-06-09 15:32:43 +0200

    Add MyFrameParameterResolver.java
    - add two frames

---
+++ src/test/java/myframe/MyFrameParameterResolver.java
@@ -0,0 +1,44 @@
+package myframe;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import texteditor.console.MyFrame;
+
+import javax.swing.*;
+import java.lang.reflect.Parameter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class MyFrameParameterResolver implements ParameterResolver {
+
+    private final Map<String, MyFrame> frames;
+
+    public MyFrameParameterResolver() {
+        Map<String, MyFrame> frames = new HashMap<>();
+        frames.put("Large Frame", new MyFrame(new JFrame("Large Frame"), new JTextArea(), 1000, 1000));
+        frames.put("Small Frame", new MyFrame(new JFrame("Small Frame"), new JTextArea(), 50, 50));
+        this.frames = frames;
+    }
+
+    @Override
+    public boolean supportsParameter(final ParameterContext parameterContext,
+                                     final ExtensionContext extensionContext) throws ParameterResolutionException {
+        Parameter parameter = parameterContext.getParameter();
+        return Objects.equals(parameter.getParameterizedType().
+                getTypeName(), "java.util.Map<java.lang.String, texteditor.console.MyFrame>");
+    }
+
+    @Override
+    public Object resolveParameter(final ParameterContext parameterContext,
+                                   final ExtensionContext extensionContext) throws ParameterResolutionException {
+        ExtensionContext.Store store = extensionContext.getStore(ExtensionContext.Namespace.create(MyFrame.class));
+        return store.getOrComputeIfAbsent("frames", f -> getFrames());
+    }
+
+    private Map<String, MyFrame> getFrames() {
+        return frames;
+    }
+}
