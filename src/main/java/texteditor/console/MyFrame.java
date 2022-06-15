87be4e691938b4128a151baf77b3c2e5aea6385d
petterBT <petter.bergstrom@iths.se>
2022-06-15 18:05:04 +0200

    Refactor MyFrame.java
    - remove instance variables
    - remove methods
    - add size-parameters to frame in constructor

--- src/main/java/texteditor/console/MyFrame.java
+++ src/main/java/texteditor/console/MyFrame.java
@@ -9,39 +9,11 @@ public class MyFrame {
 
     private JFrame frame;
     private JTextArea textArea;
-    private int width;
-    private int height;
 
-    /**
-     * Constructor that initializes all fields.
-     * @param frame Represents the main window to which components like menus, text-fields, buttons etc. are added.
-     * @param textArea The text-field where all the writing and editing is done in the text-editor.
-     * @param width Starting width of the console window.
-     * @param height Starting height of the console window.
-     */
-    public MyFrame(JFrame frame, JTextArea textArea, int width, int height) {
+    public MyFrame(JFrame frame, JTextArea textArea) {
         this.frame = frame;
         this.textArea = textArea;
-        this.width = width;
-        this.height = height;
-    }
-
-    /**
-     * Method that first adds menubar, text-field and size to the frame, then creates the GUI.
-     * @param menuBar Is the menubar that holds File-menu, Edit-menu and Close-button
-     */
-    public void buildFrame(JMenuBar menuBar) {
-        frame.setJMenuBar(menuBar);
-        frame.add(textArea);
-        frame.setSize(width, height);
-    }
-
-    /**
-     * Method that controls whether the GUI is displayed on the screen.
-     * @param visible If the value of 'visible' is set to True, the frame is displayed.
-     */
-    public void show(boolean visible) {
-        frame.setVisible(visible);
+        this.frame.add(textArea);
     }
 
     public JFrame getFrame() {
@@ -55,24 +27,7 @@ public class MyFrame {
     public JTextArea getTextArea() {
         return textArea;
     }
-
     public void setTextArea(JTextArea textArea) {
         this.textArea = textArea;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
     }
 }
