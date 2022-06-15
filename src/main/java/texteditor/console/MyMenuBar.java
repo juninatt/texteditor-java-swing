b646a4b27cb42fcfdd3175fc95d9421fd8000dd1
petterBT <petter.bergstrom@iths.se>
2022-06-15 18:54:11 +0200

    Refactor MyMenuBar.java
    - remove instance variables
    - refactor methods
    - add components to menu-bar in constructor

--- src/main/java/texteditor/console/MyMenuBar.java
+++ src/main/java/texteditor/console/MyMenuBar.java
@@ -1,6 +1,7 @@
 package texteditor.console;
 
 import javax.swing.*;
+import java.util.Set;
 
 /**
  * Holds the values that describe the menubar that is applied to the text-editor.
@@ -8,31 +9,17 @@ import javax.swing.*;
 public class MyMenuBar {
 
     private JMenuBar menuBar;
-    private JMenu menuFile;
-    private JMenu menuEdit;
-    private JMenuItem close;
 
-    /**
-     * Constructor that initializes all fields.
-     * @param menuBar  Is the menubar that holds File-menu, Edit-menu and Close-button.
-     * @param menuFile Is the menu that holds the File-options.
-     * @param menuEdit Is the menu that holds the Edit-options.
-     * @param close Is the component that act like the Close-button on the menubar
-     */
-    public MyMenuBar(JMenuBar menuBar, JMenu menuFile, JMenu menuEdit, JMenuItem close) {
+    public MyMenuBar(JMenuBar menuBar, Set<JMenu> menus, Set<JMenuItem> menuItems) {
         this.menuBar = menuBar;
-        this.menuFile = menuFile;
-        this.menuEdit = menuEdit;
-        this.close = close;
+        addMenus(menus);
+        addMenuItems(menuItems);
     }
-
-    /**
-     * Method that adds File-menu, Edit-menu and Close-button to menubar
-     */
-    public void addBars() {
-        menuBar.add(menuFile);
-        menuBar.add(menuEdit);
-        menuBar.add(close);
+    private void addMenus(Set<JMenu> menuSet) {
+        menuSet.forEach(this.menuBar::add);
+    }
+    private void addMenuItems(Set<JMenuItem> itemSet) {
+        itemSet.forEach(this.menuBar::add);
     }
 
     public JMenuBar getMenuBar() {
@@ -43,27 +30,4 @@ public class MyMenuBar {
         this.menuBar = menuBar;
     }
 
-    public JMenu getMenuFile() {
-        return menuFile;
-    }
-
-    public void setMenuFile(JMenu menuFile) {
-        this.menuFile = menuFile;
-    }
-
-    public JMenu getMenuEdit() {
-        return menuEdit;
-    }
-
-    public void setMenuEdit(JMenu menuEdit) {
-        this.menuEdit = menuEdit;
-    }
-
-    public JMenuItem getClose() {
-        return close;
-    }
-
-    public void setClose(JMenuItem close) {
-        this.close = close;
-    }
 }
