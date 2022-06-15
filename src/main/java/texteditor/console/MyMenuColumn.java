a1c6741b7f847f92afe43cca2add2bff0b925fb9
petterBT <petter.bergstrom@iths.se>
2022-06-15 18:56:27 +0200

    Refactor MyMenuColumn.java
    - make method 'connectOptionsToMenu' private
    - call 'connectOptionsToMenu' inside constructor

--- src/main/java/texteditor/console/MyMenuColumn.java
+++ src/main/java/texteditor/console/MyMenuColumn.java
@@ -12,23 +12,14 @@ public class MyMenuColumn {
     private Set<JMenuItem> menuItems;
     private Set<String> menuOptions;
 
-    /**
-     * Constructor that initializes all fields.
-     * @param menu Represents a pull-down-menu assigned to the menu bar
-     * @param menuItems Is the list of components to fill the menu, that the user can interact with.
-     * @param menuOptions Is the list of options that will be applied to the components in the menu.
-     */
     public MyMenuColumn(JMenu menu, Set<JMenuItem> menuItems, Set<String> menuOptions) {
         this.menu = menu;
         this.menuItems = menuItems;
         this.menuOptions = menuOptions;
+        connectOptionsToMenu();
     }
 
-    /**
-     * Method that iterates Set{@link #menuOptions} and for each option creates a new component
-     * to which it applies its value and adds to Set{@link #menuItems}.
-     */
-    public void connectOptionsToMenu() {
+    private void connectOptionsToMenu() {
         menuOptions.forEach(option -> menuItems.add(new JMenuItem(option)));
         menuItems.forEach(menu::add);
     }
